5959b7f28dff9abadea7f6ba8e4778f8e9a8150d
Cory Westerfield <cory.westerfield@gmail.com>
2014-09-09 10:22:11 -0600

    User can see a list of todo items

--- spec/features/workflow_spec.rb
+++ spec/features/workflow_spec.rb
@@ -4,17 +4,30 @@ end
 
 include Capybara::DSL
 
-Capybara.configure do |config|
-  config.run_server = false
-  config.current_driver = :webkit
-  config.app_host = 'http://localhost:8080'
-  config.javascript_driver = :webkit
-end
-
 describe 'visiting the homepage' do
+  before do
+    Capybara.configure do |config|
+      config.run_server = false
+      config.current_driver = :webkit
+      config.app_host = 'http://localhost:8080'
+      config.javascript_driver = :webkit
+    end
+  end
+
   it 'allows the user to visit the homepage' do
     visit '/'
 
-    expect(page).to have_content "Hello world"
+    expect(page).to have_content 'Hello world'
+  end
+
+  it 'allows the user to see a list of todo items' do
+    visit '/'
+
+    expect(page).to have_css 'div#todo_items'
+    expect(page).to have_content 'Cat'
+    expect(page).to have_content 'Dog'
+    expect(page).to have_content 'Giraffe'
+    expect(page).to have_content 'Zebra'
+    expect(page).to have_content 'Elephant'
   end
 end
